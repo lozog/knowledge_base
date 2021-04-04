2c3938b62b3c9ccc7b27e2293097ace62fe538b6
Liam Ozog <liam@ada.support>
2021-04-04 00:45:13 -0400

    install pymongo, set up basic route to create documents

--- api/app.py
+++ api/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+
+from db import db
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/documents', methods=["POST"])
+def create_document():
+    documents = db.documents
+    document = {
+        "title": "test document",
+        "content": "lorem ipsum",
+        "parent": None,
+        "children": []
+    }
+    document_id = documents.insert_one(document).inserted_id
+    return f"Inserted as {document_id}"
